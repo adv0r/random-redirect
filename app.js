56ba45f8cbceb0c3c2e0143fd6b9a88d5ad85a08
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2012-07-29 22:58:25 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+    list=fs.readFileSync('sitelist.txt').toString().split("\n");
+    rnd = Math.floor(Math.random()*list.length);
+  	response.writeHead(200, {"Content-Type": "text/html"});
+  	response.end(getHtmlPage(list[rnd]));
+});
+
+server.listen(8000);
+console.log("Server running at http://127.0.0.1:8000/");
+
+function getHtmlPage(url)
+{
+	return 	"<html><head><meta HTTP-EQUIV='REFRESH' content='1; url="+url+"'></head>"+
+			"<body>So long and thanks for all the fish</body></html>";				
+}
+
