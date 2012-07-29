51420bd7fb5da7012265ad7a9418e527a05d1209
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2012-07-30 01:02:22 +0200

    custom message

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 // Load the http module to create an http server.
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 
 var server = http.createServer(function (request, response) {
-    list=fs.readFileSync('sitelist.txt').toString().split("\n");
-    rnd = Math.floor(Math.random()*list.length);
-  	response.writeHead(200, {"Content-Type": "text/html"});  	
-  	response.end(getHtmlPage(list[rnd]));
+	if(request.method=='GET' && request.url!="/favicon.ico") {
+		var signature = url.parse(request.url,true);
+		var nick = "@"+signature.query.handle;
+  	    list=fs.readFileSync('sitelist.txt').toString().split("\n");
+        rnd = Math.floor(Math.random()*list.length);
+  	    response.writeHead(200, {"Content-Type": "text/html"});  	
+  	    response.end(getHtmlPage(list[rnd],nick));
+  	}
 });
 
 server.listen(8000);
 console.log("Server running at http://127.0.0.1:8000/");
 
-function getHtmlPage(url)
+function getHtmlPage(url,nick)
 {
-	console.log("requested URL : "+url);
+	console.log("nick : " + nick + ", requested URL : "+url);
 	return 	"<html><head><meta HTTP-EQUIV='REFRESH' content='7; url="+url+"'></head>"+
-			"<body><h1>Thank you for following me ;) </h1> "+
-				"<h2>Now you will be redirected to a random link I like. Have fun</h2>"+
+			"<body><h1>Hey "+nick+" thank you for following, you are awesome </h1> "+
+				"<h2>Now you will be redirected to a juicy random link. Have fun</h2>"+
 				"<h3>@adv4nced</h3>"+
 				"</body></html>";		
 }
