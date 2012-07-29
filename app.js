c56be97292579760f5093f8056345f3d2608d164
Nicolò Paternoster <paternoster.nicolo@gmail.com>
2012-07-30 01:09:50 +0200

    refinement

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var server = http.createServer(function (request, response) {
 	if(request.method=='GET' && request.url!="/favicon.ico") {
 		var signature = url.parse(request.url,true);
 		var nick = "@"+signature.query.handle;
+		if (nick=="@undefined") nick ="";
   	    list=fs.readFileSync('sitelist.txt').toString().split("\n");
         rnd = Math.floor(Math.random()*list.length);
   	    response.writeHead(200, {"Content-Type": "text/html"});  	
@@ -21,8 +22,8 @@ function getHtmlPage(url,nick)
 {
 	console.log("nick : " + nick + ", requested URL : "+url);
 	return 	"<html><head><meta HTTP-EQUIV='REFRESH' content='7; url="+url+"'></head>"+
-			"<body><h1>Hey "+nick+" thank you for following, you are awesome </h1> "+
-				"<h2>Now you will be redirected to a juicy random link. Have fun</h2>"+
+			"<body><h1>Hey "+nick+" , thank you for following!</h1> "+
+				"<h2>To show my gratitude, now you will be redirected to a juicy-random-nerdy link.</h2>"+
 				"<h3>@adv4nced</h3>"+
 				"</body></html>";		
 }
